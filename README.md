5224db6bff4f4b200ade81ee629986d4904a926a
Hugh Haworth <hughelliotclyde@gmail.com>
2020-09-14 17:28:42 +1200

    initialise project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plantr - time to grow some VEGES 
+
+## A gardening app for all your gardening needs
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Plantr</title>
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+    </head>
+    <body>
+    <h1>Plantr 2.0</h1>
+    </body>
+</html>
