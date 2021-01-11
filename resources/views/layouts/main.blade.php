c58bf6abcee6342533a82c5604b9a2e6a7f6fdcd
Hugh Haworth <hughelliotclyde@gmail.com>
2021-01-12 09:36:56 +1300

    Added meta tags for social media sharing

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Plantr: an app for growing and harvesting vegetables in New Zealand.">
+        <meta property="og:title" content="Plantr - @yield('title')">
+        <meta property="og:description" content="Plantr: an app for growing and harvesting vegetables in New Zealand.">
+        <meta property="og:image" content="images/plantrpreview.jpg">
+        <meta property="og:url" content="{{env('APP_URL')}}">
+        <meta name="twitter:card" content="summary_large_image">
         <title>Plantr - @yield('title')</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon">
         <link rel="preload" href="fonts/Aadam.otf" as="font" crossorigin="anonymous">
